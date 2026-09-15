6d6a6c30d6bb49b4016957bed6f0d9934a1c67af
김태균 <taekyun0525@gmail.com>
2026-09-15 20:07:00 +0900

    first commit

---
+++ src/test/java/com/example/gildongbe/GilDongBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gildongbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GilDongBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
